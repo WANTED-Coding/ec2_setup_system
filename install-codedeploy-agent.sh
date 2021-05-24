0b448963db05665f1ac2577282a033d54320b183
Dao Hong Vinh <lambiengcode@gmail.com>
2021-05-24 20:55:55 +0700

    Update install-codedeploy-agent.sh

--- install-codedeploy-agent.sh
+++ install-codedeploy-agent.sh
@@ -6,4 +6,5 @@ cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
+#20.04 sudo ./install auto > /tmp/logfile
 sudo service codedeploy-agent status
