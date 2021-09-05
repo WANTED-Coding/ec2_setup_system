5642cdb23621443e4af4d41334f7185755e4292c
Dao Hong Vinh <lambiengcode@gmail.com>
2021-09-05 17:22:20 +0700

    feat(Version): install node 14.x

--- install-environment.sh
+++ install-environment.sh
@@ -1,4 +1,4 @@
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install nodejs
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
